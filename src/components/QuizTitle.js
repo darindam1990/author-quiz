bb66d75219d7cb3e436afa6d8dfff39098513e1f
Arindam Das <adas1990.gatech@gmail.com>
2019-06-08 21:20:54 -0700

    text changes

--- src/components/QuizTitle.js
+++ src/components/QuizTitle.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const QuizTitle = (state) => {
     return <div className="QuizTitle">
-        <span>Select the option that correctly matches the work of the author pictured below</span>
+        <span>Select the option that correctly matches a literary work of the author pictured below</span>
         <span style={ {marginLeft: 'auto', fontSize: '2em'} }>{state.current} / {state.total}</span>
     </div>;
 };
